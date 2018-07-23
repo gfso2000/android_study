57832375693e35be0a221512eb0e23e868f616fb
i325639 <jack.yu05@sap.com>
2018-07-23 17:27:34 +0800

    goods overview

--- OAuthClient/app/src/main/java/com/gfso/client/oauthclientapplication/fragment/GoodsOverviewFragment.java
+++ OAuthClient/app/src/main/java/com/gfso/client/oauthclientapplication/fragment/GoodsOverviewFragment.java
@@ -1,32 +1,57 @@
 package com.gfso.client.oauthclientapplication.fragment;
 
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.gfso.client.oauthclientapplication.R;
+import com.gfso.client.oauthclientapplication.bean.RecommendGoodsBean;
+import com.gfso.client.oauthclientapplication.fragment.recycleview.GoodsDetailRecommendAdapter;
+import com.gfso.client.oauthclientapplication.fragment.widget.NetworkImageHolderView;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class GoodsOverviewFragment extends Fragment {
-    @BindView(R.id.tv_goods_config)
-    TextView tv_goods_config;
+public class GoodsOverviewFragment extends GoodsDetailFragment {
+    @BindView(R.id.fab_up_slide)
+    FloatingActionButton fab_up_slide;
+    @BindView(R.id.vp_item_goods_img)
+    ConvenientBanner vp_item_goods_img;
+    @BindView(R.id.vp_recommend)
+    ConvenientBanner vp_recommend;
+    @BindView(R.id.sv_goods_info)
+    ScrollView sv_goods_info;
+    @BindView(R.id.ll_current_goods)
+    LinearLayout ll_current_goods;
+    @BindView(R.id.ll_activity)
+    LinearLayout ll_activity;
+    @BindView(R.id.ll_comment)
+    LinearLayout ll_comment;
+    @BindView(R.id.ll_pull_up)
+    LinearLayout ll_pull_up;
+    @BindView(R.id.tv_old_price)
+    TextView tv_old_price;
 
     AppCompatActivity activity = null;
 
     public GoodsOverviewFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,7 +61,123 @@ public class GoodsOverviewFragment extends Fragment {
         activity = (AppCompatActivity)this.getActivity();
         ButterKnife.bind(this, view);
 
+        setHeadImagesView();
+        setRecommendGoods();
+        //设置文字中间一条横线
+        tv_old_price.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        fab_up_slide.hide();
+
+        fab_up_slide.setOnClickListener(this);
+        ll_current_goods.setOnClickListener(this);
+        ll_activity.setOnClickListener(this);
+        ll_comment.setOnClickListener(this);
+        ll_pull_up.setOnClickListener(this);
+
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_pull_up:
+                //上拉查看图文详情
+                //sv_switch.smoothOpen(true);
+                break;
+
+            case R.id.fab_up_slide:
+                //点击滑动到顶部
+                sv_goods_info.smoothScrollTo(0, 0);
+                //sv_switch.smoothClose(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 给商品轮播图设置图片路径
+     */
+    private void setHeadImagesView() {
+        List<String> imgUrls = new ArrayList<>();
+        imgUrls.add("http://img4.hqbcdn.com/product/79/f3/79f3ef1b0b2283def1f01e12f21606d4.jpg");
+        imgUrls.add("http://img14.hqbcdn.com/product/77/6c/776c63e6098f05fdc5639adc96d8d6ea.jpg");
+        imgUrls.add("http://img13.hqbcdn.com/product/41/ca/41cad5139371e4eb1ce095e5f6224f4d.jpg");
+        imgUrls.add("http://img10.hqbcdn.com/product/fa/ab/faab98caca326949b87b770c8080e6cf.jpg");
+        imgUrls.add("http://img2.hqbcdn.com/product/6b/b8/6bb86086397a8cd0525c449f29abfaff.jpg");
+        //初始化商品图片轮播
+        vp_item_goods_img.setPages(new CBViewHolderCreator() {
+            @Override
+            public Object createHolder() {
+                return new NetworkImageHolderView();
+            }
+        }, imgUrls);
+        //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+        vp_item_goods_img.setPageIndicator(new int[]{R.mipmap.index_white, R.mipmap.index_red});
+        vp_item_goods_img.setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.CENTER_HORIZONTAL);
+    }
+
+    /**
+     * 设置推荐商品
+     */
+    public void setRecommendGoods() {
+        List<RecommendGoodsBean> data = new ArrayList<>();
+        data.add(new RecommendGoodsBean("Letv/乐视 LETV体感-超级枪王 乐视TV超级电视产品玩具 体感游戏枪 电玩道具 黑色",
+                "http://img4.hqbcdn.com/product/79/f3/79f3ef1b0b2283def1f01e12f21606d4.jpg", new BigDecimal(599), "799"));
+        data.add(new RecommendGoodsBean("IPEGA/艾派格 幽灵之子 无线蓝牙游戏枪 游戏体感枪 苹果安卓智能游戏手柄 标配",
+                "http://img2.hqbcdn.com/product/00/76/0076cedb0a7d728ec1c8ec149cff0d16.jpg", new BigDecimal(299), "399"));
+        data.add(new RecommendGoodsBean("Letv/乐视 LETV体感-超级枪王 乐视TV超级电视产品玩具 体感游戏枪 电玩道具 黑色",
+                "http://img4.hqbcdn.com/product/79/f3/79f3ef1b0b2283def1f01e12f21606d4.jpg", new BigDecimal(599), "799"));
+        data.add(new RecommendGoodsBean("IPEGA/艾派格 幽灵之子 无线蓝牙游戏枪 游戏体感枪 苹果安卓智能游戏手柄 标配",
+                "http://img2.hqbcdn.com/product/00/76/0076cedb0a7d728ec1c8ec149cff0d16.jpg", new BigDecimal(299), "399"));
+        List<List<RecommendGoodsBean>> handledData = handleRecommendGoods(data);
+        //设置如果只有一组数据时不能滑动
+        vp_recommend.setManualPageable(handledData.size() == 1 ? false : true);
+        vp_recommend.setCanLoop(handledData.size() == 1 ? false : true);
+        vp_recommend.setPages(new CBViewHolderCreator() {
+            @Override
+            public Object createHolder() {
+                return new GoodsDetailRecommendAdapter();
+            }
+        }, handledData);
+        vp_recommend.setPageIndicator(new int[]{R.drawable.shape_item_index_white, R.drawable.shape_item_index_red});
+        vp_recommend.setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.CENTER_HORIZONTAL);
+    }
+
+    /**
+     * 处理推荐商品数据(每两个分为一组)
+     *
+     * @param data
+     * @return
+     */
+    public static List<List<RecommendGoodsBean>> handleRecommendGoods(List<RecommendGoodsBean> data) {
+        List<List<RecommendGoodsBean>> handleData = new ArrayList<>();
+        int length = data.size() / 2;
+        if (data.size() % 2 != 0) {
+            length = data.size() / 2 + 1;
+        }
+        for (int i = 0; i < length; i++) {
+            List<RecommendGoodsBean> recommendGoods = new ArrayList<>();
+            for (int j = 0; j < (i * 2 + j == data.size() ? 1 : 2); j++) {
+                recommendGoods.add(data.get(i * 2 + j));
+            }
+            handleData.add(recommendGoods);
+        }
+        return handleData;
+    }
+
+    // 开始自动翻页
+    @Override
+    public void onResume() {
+        super.onResume();
+        //开始自动翻页
+        vp_item_goods_img.startTurning(4000);
+    }
+
+    // 停止自动翻页
+    @Override
+    public void onPause() {
+        super.onPause();
+        //停止翻页
+        vp_item_goods_img.stopTurning();
+    }
 }
